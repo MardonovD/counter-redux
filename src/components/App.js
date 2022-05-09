bc8ef357fc5b5c4120d29763bb622633fbc50f95
Dostonbek <mardonovdoston69@gmail.com>
2022-05-09 21:51:20 +0300

    create counter.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,14 @@
-import { type } from "@testing-library/user-event/dist/type";
 import React from "react";
-import { createStore } from "redux";
-import holatlar from "../redux/Reducers";
-import { inc, dec } from "../redux/Action.js";
-const store = createStore(holatlar);
-const { dispatch, getState } = store;
-function App() {
-  const incrementClick = () => {
-    dispatch(inc());
-    document.querySelector(".nol").textContent = getState().value;
-  };
-  const decrementClick = () => {
-    dispatch(dec());
-    document.querySelector(".nol").textContent = getState().value;
-  };
 
-  console.log(store);
-  return (
-    <div className="App" style={{ marginLeft: "500px", marginTop: "50px" }}>
-      <div className=" ui  container ">
-        <div className="counter-div">
-          <h1 className="counter">
-            Counter:<span className="nol">0</span>
-          </h1>
-        </div>
-        <div className="btns">
-          <button
-            onClick={incrementClick}
-            className="btn btn-dark me-2"
-            id="increment"
-          >
-            Increment -
-          </button>
-          <button
-            onClick={decrementClick}
-            className="btn btn-danger"
-            id="decrement"
-          >
-            Decrement +
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+import Counter from "./Counter";
+
+function App() {
+
+
+  // return { incrementClick, decrementClick };
+
+  // console.log(store);
+  return <Counter />;
 }
 
 export default App;
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,45 @@
 import React from "react";
+import { inc, dec } from "../redux/Action.js";
+import { createStore } from "redux";
+import holatlar from "../redux/Reducers";
+const store = createStore(holatlar);
+const { dispatch, getState } = store;
 function Counter(props) {
-  return <div></div>;
+  const incrementClick = () => {
+    dispatch(inc());
+    document.querySelector(".nol").textContent = getState().value;
+  };
+  const decrementClick = () => {
+    dispatch(dec());
+    document.querySelector(".nol").textContent = getState().value;
+  };
+  return (
+    <div className="App" style={{ marginLeft: "500px", marginTop: "50px" }}>
+      <div className=" ui  container ">
+        <div className="counter-div">
+          <h1 className="counter">
+            Counter:<span className="nol">0</span>
+          </h1>
+        </div>
+        <div className="btns">
+          <button
+            onClick={incrementClick}
+            className="btn btn-dark me-2"
+            id="increment"
+          >
+            Increment -
+          </button>
+          <button
+            onClick={decrementClick}
+            className="btn btn-danger"
+            id="decrement"
+          >
+            Decrement +
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Counter;
